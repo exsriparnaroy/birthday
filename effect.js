6b9834a9505d0ba857c95b1a9eedbba0aa958658
exsriparnaroy <93369438+exsriparnaroy@users.noreply.github.com>
2022-09-05 12:07:59 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -176,11 +176,8 @@ $('document').ready(function(){
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
                         $("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-				         window.close();
-				});
-				
+			if(i==4){
+				window.close();				
 			}
 			
 			else{
